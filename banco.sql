8519a022ead133c03fda29c81e2d2c6021f37e77
josea95 <joseademir.foz@gmail.com>
2025-04-13 15:48:38 -0300

    SQL Atualizado de acordo com a documentacao

--- banco.sql
+++ banco.sql
@@ -1,4 +1,5 @@
 -- TABELA USUARIOS --
+
 create table tb_usuarios(
 id int primary key,
 nome varchar(100),
@@ -7,10 +8,9 @@ email varchar(100)
 );
 
 -- TABELA  FILA PEDIDOS--
+
 create table tb_fila_pedidos(
 id int primary key,
-itens_pedido text,
-nome_cliente varchar(100),
 data_pedido date,
 hora_pedido time,
 id_usuario int,
@@ -20,10 +20,19 @@ observacao text,
 foreign key (id_usuario) references tb_usuarios(id)
 );
 
+--TABELA PRODUTOS PEDIDOS --
+
+create table tb_produtos_pedidos(
+    id int primary key,
+    id_pedidos  int,
+    id_produtos int,
+    foreign key (id_pedidos) references tb_fila_pedidos(id),
+    foreign key (id_produtos) references tb_produtos(id)
+);
 -- TABELA HISTORICO PEDIDOS --
-create table historico_pedidos(
+
+create table tb_historico_pedidos(
 id int primary key,
-itens_pedido text,
 nome_cliente varchar(100),
 data_pedido date,
 hora_pedido time,
@@ -42,4 +51,13 @@ descricao text,
 preco float,
 data_criacao date,
 data_atualizacao date
+);
+
+-- TABELA PRODUTO HISTORICO PEDIDOS --
+create table tb_produtos_historico_pedidos(
+id int primary key,
+id_historico_pedido int,
+id_produto int,
+foreign key (id_historico_pedido) references tb_produtos_historico_pedidos(id),
+foreign key (id_produto) references tb_produtos(id)
 );
