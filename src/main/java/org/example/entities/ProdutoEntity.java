6b5fb927f5c8be0f0b92c473dbf9b5f51ea90a22
xxMarina <marinapiresramos@gmail.com>
2025-04-14 15:07:35 -0300

    Adicionando as entidades

---
+++ src/main/java/org/example/entities/ProdutoEntity.java
@@ -0,0 +1,104 @@
+package org.example.entities;
+
+import javax.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_produtos")
+public class ProdutoEntity {
+
+    public ProdutoEntity() {
+
+    }
+
+    public ProdutoEntity(String nome, String descricao, Double preco, String dataCriacao, String dataAtualizacao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "categoria")
+    private String categoria;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    private Double preco;
+
+    @Column(name = "data_criacao")
+    private LocalDate dataCriacao;
+
+    @Column(name = "data_atualizacao")
+    private LocalDate dataAtualizacao;
+
+    @OneToMany(mappedBy = "produto")
+    private List<ProdutoPedidoEntity> produtosPedidos;
+
+    //Setters and Getters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDate getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDate dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+}
