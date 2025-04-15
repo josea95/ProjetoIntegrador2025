cc2076e03afff280b797c80ca7c69df19d203365
unknown <jhoranygor13@gmail.com>
2025-04-15 02:49:09 -0300

    Usuario Service

--- src/main/java/org/example/services/UsuarioService.java
+++ src/main/java/org/example/services/UsuarioService.java
@@ -1,14 +1,47 @@
 package org.example.services;
+
 import org.example.entities.UsuarioEntity;
 import org.example.repository.UsuarioRepository;
-import javax.persistence.EntityManager;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 public class UsuarioService {
-private UsuarioService usuarioService;
-public UsuarioService(EntityManager em) {
-    this.usuarioService = new UsuarioService(em);
-}
 
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioService(EntityManager em) {
+        this.usuarioRepository = new UsuarioRepository(em);
+    }
+
+    public void criarUsuario(String nome, String email, String login, String senha) {
+        UsuarioEntity usuario = new UsuarioEntity();
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setLogin(login);
+        usuario.setSenha(senha);
+
+        usuarioRepository.salvar(usuario);
+    }
+
+    public UsuarioEntity buscarPorId(int id) {
+        return usuarioRepository.buscarPorId(id);
+    }
+
+    public List<UsuarioEntity> listarTodos() {
+        return usuarioRepository.listarTodos();
+    }
+
+    public boolean deletarUsuarioPorId(int id) {
+        UsuarioEntity usuario = usuarioRepository.buscarPorId(id);
+        if (usuario != null) {
+            usuarioRepository.deletar(usuario);
+            return true;
+        }
+        return false;
+    }
+
+    public UsuarioEntity login(String login, String senha) {
+        return usuarioRepository.buscarPorLoginESenha(login, senha);
+    }
 }
