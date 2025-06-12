9c86ea7679486b7f3d93b317e0a3777ea7870bef
xxMarina <marinapiresramos@gmail.com>
2025-06-12 11:36:31 -0300

    Adicionando nos botoes os metodos para pesquisar e cancelar um pedido

--- src/main/java/org/example/Swing/TelaCancelarPedidoSwing.java
+++ src/main/java/org/example/Swing/TelaCancelarPedidoSwing.java
@@ -1,11 +1,26 @@
 package org.example.Swing;
 
+import org.example.controller.FilaPedidoController;
+import org.example.model.repository.FilaPedidoRepository;
+import org.example.model.services.FilaPedidoService;
+
+import org.example.model.util.CustomizerFactory;
+import javax.persistence.EntityManager;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class TelaCancelarPedidoSwing extends JFrame {
+    FilaPedidoController filaPedidoController;
+    FilaPedidoService filaPedidoService;
+    FilaPedidoRepository filaPedidoRepository;
+    EntityManager em = CustomizerFactory.getEntityManager();
 
     public TelaCancelarPedidoSwing() {
+        this.filaPedidoRepository = new FilaPedidoRepository(em);
+        this.filaPedidoService = new FilaPedidoService(em);
+        this.filaPedidoController = new FilaPedidoController(filaPedidoService);
+
         setTitle("Cancelar Pedido");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 500);
@@ -23,17 +38,21 @@ public class TelaCancelarPedidoSwing extends JFrame {
         txtId.setBounds(162, 101, 200, 30);
         getContentPane().add(txtId);
 
-        JButton btnCancelar = new JButton("Cancelar Pedido");
-        btnCancelar.setFont(new Font("Tahoma", Font.PLAIN, 12));
-        btnCancelar.setBounds(531, 411, 145, 30);
-        getContentPane().add(btnCancelar);
+        JButton cancelarButton = new JButton("Cancelar Pedido");
+        cancelarButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        cancelarButton.addActionListener(e -> {
+            String senhaPedido = txtId.getText();
+                filaPedidoController.cancelarPedido(senhaPedido);
+
+        });
+        cancelarButton.setBounds(531, 411, 145, 30);
+        getContentPane().add( cancelarButton );
 
         JButton btnVoltar = new JButton("Voltar");
         btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 12));
         btnVoltar.setBounds(10, 411, 145, 30);
         getContentPane().add(btnVoltar);
 
-        // Ação do botão voltar
         btnVoltar.addActionListener(e -> dispose());
     }
 
