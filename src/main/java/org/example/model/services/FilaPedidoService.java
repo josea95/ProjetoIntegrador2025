c3f3dc7086f0da230f3b8d41366056e0f6cc4527
xxMarina <marinapiresramos@gmail.com>
2025-06-12 11:42:25 -0300

    Adptando para Swing, e colocando os avisos do JOptionPane

--- src/main/java/org/example/model/services/FilaPedidoService.java
+++ src/main/java/org/example/model/services/FilaPedidoService.java
@@ -6,6 +6,7 @@ import org.example.model.enums.StatusPedido;
 import org.example.model.repository.FilaPedidoRepository;
 
 import javax.persistence.EntityManager;
+import javax.swing.*;
 import java.util.List;
 
 public class FilaPedidoService {
@@ -13,35 +14,45 @@ public class FilaPedidoService {
     private final FilaPedidoRepository filaRepo;
 
     public FilaPedidoService(EntityManager em) {
-        this.filaRepo = new FilaPedidoRepository(em);
-
+        this.filaRepo = new FilaPedidoRepository( em );
     }
 
     public List<FilaPedidoEntity> listarFilaPedidos() {
         return filaRepo.listarTodos();
     }
 
-    public FilaPedidoEntity pesquisarPedido(String senha) {
-        return filaRepo.buscarPorSenha(senha);
+    public void pesquisarPedido(String senha) {
+        FilaPedidoEntity pedido = filaRepo.buscarPorSenha(senha);
+        if (pedido == null) {
+            JOptionPane.showMessageDialog(null, "Pedido não encontrado.");
+        } else {
+            String msg = "Senha: " + pedido.getSenhaPedido() +
+                    "\nData: " + pedido.getDataPedido() +
+                    "\nHora: " + pedido.getHoraPedido() +
+                    "\nStatus: " + pedido.getStatusPedido().toString() +
+                    "\nUsuário: " + pedido.getUsuario().getNome();
+            JOptionPane.showMessageDialog(null, msg);
+        }
     }
 
     public boolean cancelarPedido(String senha) {
-        FilaPedidoEntity pedido = filaRepo.buscarPorSenha(senha);
+        FilaPedidoEntity pedido = filaRepo.buscarPorSenha( senha );
 
         if (pedido == null) {
+            JOptionPane.showMessageDialog( null, "Pedido não encontrado." );
             return false;
         }
-
         if (pedido.getStatusPedido() != StatusPedido.FILA) {
+            JOptionPane.showMessageDialog( null, "Pedido não pode ser cancelado. Status inválido: " + pedido.getStatusPedido() );
             return false;
         }
-
-        filaRepo.deletar(pedido);
+        JOptionPane.showMessageDialog( null, "Pedido cancelado com sucesso!" );
+        filaRepo.deletar( pedido );
         return true;
     }
 
     public List<FilaPedidoEntity> verHistoricoPedidos(UsuarioEntity usuarioLogado) {
-        return filaRepo.listarPorUsuario(usuarioLogado);
+        return filaRepo.listarPorUsuario( usuarioLogado );
     }
 }
 
